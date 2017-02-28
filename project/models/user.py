d9fec764d0dcc1c7b46bdccc0823799d07353d7c
Mohammad Amin Haghpanah <mdan.hagh@gmail.com>
2017-02-28 20:41:12 +0330

    remove EmailField

--- project/models/user.py
+++ project/models/user.py
@@ -10,7 +10,7 @@ from project.extensions import db
 
 class User(db.Document):
     username = db.StringField(required=True, unique=True)
-    email = db.EmailField(required=True, unique=True)
+    email = db.StringField(required=True, unique=True)
     password = db.StringField(required=True)
 
 
