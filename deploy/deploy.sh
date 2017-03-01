5e85c34e3d904fa44e53796ae1d7487dca855c5a
Mohammad Amin Haghpanah <mdan.hagh@gmail.com>
2017-03-01 20:48:37 +0330

    rename image and container

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 
-docker build -f Dockerfile -t myimage ../
-docker run -d --name mycontainer -p 443:80 myimage
+docker build -f Dockerfile -t ijust_image ../
+docker run -d --name ijust_container -p 443:80 ijust_image
