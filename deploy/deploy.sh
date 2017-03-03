5e2ee5eacb5811a155c09ad877b55997add93efe
Mohammad Amin Haghpanah <mdan.hagh@gmail.com>
2017-03-03 23:49:19 +0330

    add docker-compose

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-docker build -f Dockerfile -t ijust_image ../
-docker run -d --name ijust_container -v /var/www/ijust:/var/www/ijust -v /tmp:/tmp -h masterhost ijust_image
+docker build -f Dockerfile -t ijust ../
+docker-compose up -d
