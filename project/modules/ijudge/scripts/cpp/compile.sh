139cd533d2a9cc14173709071fcb83ecea390a88
Mohammad Amin Haghpanah <mdan.hagh@gmail.com>
2017-03-09 00:48:22 +0330

    fix bugs

--- project/modules/ijudge/scripts/cpp/compile.sh
+++ project/modules/ijudge/scripts/cpp/compile.sh
@@ -3,4 +3,4 @@
 TARGET=$COMPILED_DIR/a.out
 
 g++ -static -fno-optimize-sibling-calls -fno-strict-aliasing -DONLINE_JUDGE -lm -s -x c++ \
-	-O2 -o $TARGET $CODE_PATH
+	-O2 -w -o $TARGET $CODE_PATH
--- project/modules/ijudge/scripts/cpp11/compile.sh
+++ project/modules/ijudge/scripts/cpp11/compile.sh
@@ -3,4 +3,4 @@
 TARGET=$COMPILED_DIR/a.out
 
 g++ -fno-optimize-sibling-calls -fno-strict-aliasing -DONLINE_JUDGE -lm -s -x c++ \
-	-O2 -std=c++0x -o $TARGET $CODE_PATH
+	-O2 -w -std=c++0x -o $TARGET $CODE_PATH
