6772c58064c08353e19dfe1892dae08558f74fe8
Mohammad Amin Haghpanah <mdan.hagh@gmail.com>
2017-03-08 18:36:55 +0330

    add reason

--- project/modules/ijudge/__init__.py
+++ project/modules/ijudge/__init__.py
@@ -6,5 +6,5 @@ from .core import run
 
 def judge(code_path, prog_lang, testcase_dir, time_limit, space_limit):
     status, reason = run(code_path, prog_lang.name, testcase_dir, time_limit, space_limit)
-    reason = "testcase: %s" reason
+    reason = "testcase: %s" % reason
     return status, reason
