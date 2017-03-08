4d2bc1477c0016213b3bb48c09df9730798b442e
Mohammad Amin Haghpanah <mdan.hagh@gmail.com>
2017-03-08 18:41:52 +0330

    remove reason if status was accepted

--- project/modules/ijudge/__init__.py
+++ project/modules/ijudge/__init__.py
@@ -2,9 +2,11 @@
 __author__ = 'AminHP'
 
 from .core import run
+from .types import JudgementStatusType
 
 
 def judge(code_path, prog_lang, testcase_dir, time_limit, space_limit):
     status, reason = run(code_path, prog_lang.name, testcase_dir, time_limit, space_limit)
-    reason = "testcase: %s" % reason
+    if status != JudgementStatusType.Accepted:
+        reason = "testcase: %s" % reason
     return status, reason
