1fefbdc52f5f4784f14f474925d0bb2d86644921
Mohammad Amin Haghpanah <mdan.hagh@gmail.com>
2017-04-27 16:42:30 +0430

    add full installer script

---
+++ install.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+
+apt-get install -y openssl letsencrypt
+apt-get install -y docker docker.io docker-compose
+apt-get install -y nginx apache2-utils
+apt-get install -y nodejs nodejs-legacy npm
+
+service nginx stop
+service docker stop
+
+mkdir -p /var/www/ijust
+cd /var/www/ijust
+
+git clone https://gitlab.com/ijust/server
+git clone https://gitlab.com/ijust/frontend
+mkdir log
+touch reload
+
+cd frontend
+npm install -g bower
+npm install
+bower install --allow-root
+
+cd /etc/nginx/sites-enabled
+rm default
+ln -s /var/www/ijust/server/deploy/nginx.conf ijust
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+letsencrypt certonly --email=mdan.hagh@gmail.com --agree-tos -d acm.iust.ac.ir
+
+cd /var/www/ijust/server/project
+cp conf.py.sample conf.py
+
+cd /var/www
+chown www-data:www-data -R ijust
+
+service nginx start # ensure that port 80 and 443 are open
+service docker start
+
+htpasswd -c /etc/nginx/.htpasswd AminHP
+
+cd /var/www/ijust/server/deploy
+./deploy.sh
