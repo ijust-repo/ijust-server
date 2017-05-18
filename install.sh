5b8c9412f77988e8846ddd27f663f3de83e7353b
salar <salarnasiri74@gmail.com>
2017-05-18 22:12:39 +0430

    update deployment configs

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ cd /etc/nginx/sites-enabled
 rm default
 ln -s /var/www/ijust/server/deploy/nginx.conf ijust
 openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
-letsencrypt certonly --email=mdan.hagh@gmail.com --agree-tos -d acm.iust.ac.ir
+letsencrypt certonly --email=mdan.hagh@gmail.com --agree-tos -d acm.iust.ac.ir ijust.ir www.ijust.ir
 
 cd /var/www/ijust/server/project
 cp conf.py.sample conf.py
